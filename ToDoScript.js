822f10c809d986619c37a888a4125295654096ea
Bl1xvan <84408872+Bl1xvan@users.noreply.github.com>
2021-06-08 16:20:23 -0400

    Update ToDoScript.js
    Changed listitems to paragraphs

--- ToDoScript.js
+++ ToDoScript.js
@@ -1,17 +1,17 @@
-let addToDoButton = document.getElementById("addToDo");
-let inputField = document.getElementById("inputField");
-let toDoContainer = document.getElementById("toDoContainer");
+let addToDoButton = document.getElementById('addToDo');
+let inputField = document.getElementById('inputField');
+let toDoContainer = document.getElementById('toDoContainer');
 
 addToDoButton.addEventListener('click', function(){
-    var listItem = document.createElement('p');
-    listItem.classList.add('list-styling');
-    toDoContainer.appendChild(listItem);
-    listItem.innertext = inputField.value;
+    var paragraph = document.createElement('p');
+    paragraph.classList.add('paragraph-styling');
+    toDoContainer.appendChild(paragraph);
+    paragraph.innerText = inputField.value;
     inputField.value = "";
-    listItem.addEventListener('click', function(){
-        listItem.style.texDecoration = "line-through";
+    paragraph.addEventListener('click', function(){
+        paragraph.style.textDecoration = "line-through";
     })
-    listItem.addEventListener('dblclick', function(){
-        toDoContainer.removeChild(listItem);
+    paragraph.addEventListener('dblclick', function(){
+        toDoContainer.removeChild(paragraph);
     })
-})
+});
